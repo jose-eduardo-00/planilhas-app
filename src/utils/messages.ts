c59f0134d096e318d01d32eeea13ec97c2f08537
andressa-ricardo <andressaricardo30@gmail.com>
2025-03-11 23:28:46 -0300

    feat: creating a file to store messages

---
+++ src/utils/messages.ts
@@ -0,0 +1,20 @@
+export const MESSAGES = {
+  USER: {
+    CREATED: "Usuário criado com sucesso.",
+    UPDATED: "Usuário atualizado com sucesso.",
+    DELETED: "Usuário deletado com sucesso.",
+    NOT_FOUND: "Usuário não encontrado.",
+    EMAIL_EXISTS: "E-mail já cadastrado.",
+    ERROR: "Erro ao processar a solicitação.",
+    FETCHED: "Usuário(s) buscado(s) com sucesso.",
+  },
+  ERROR: {
+    INTERNAL_SERVER: "Erro interno do servidor.",
+    INVALID_REQUEST: "Requisição inválida.",
+    UNAUTHORIZED: "Não autorizado.",
+  },
+  AUTH: {
+    PASSWORD_RESET_SENT: "Código de recuperação enviado com sucesso.",
+    INVALID_CREDENTIALS: "Credenciais inválidas.",
+  },
+};
